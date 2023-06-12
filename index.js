a0242d583c79415654b1e6637cc67e65e4f4090f
SamuelWango <wangos478@gmail.com>
2023-06-12 13:38:12 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    return  `Hi, my name is ${name}.`;   
+}
+console.log(introduction("Aki"));
+
+function introductionWithLanguage(name = "Wango", language = "Python"){
+    return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Aki", "Ember.js"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguageOptional("Gracie"))
